d81f266a9c55e5bffe3aa3412dd96684bba2d8bd
Camila Sanches <camilasanches.clrs@gmail.com>
2026-07-16 22:43:44 -0300

    feat:adiciona navegação no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import {Link} from 'react-router-dom'
+
 export function Header() {
     return (
         <header className="bg-gray-800 text-white p-4 mb-4 rounded">
             <h1 className="flex justify-center items-center text-xl font-bold">iRepair</h1>
+            <nav className="flex justify-center items-center gap-4 mt-2">
+                <Link to="/" >Dashboard</Link>
+                <Link to="/clients" >Clients</Link>
+                <Link to="/service-orders" >Service Orders</Link>
+            </nav>
         </header>
     );
 }

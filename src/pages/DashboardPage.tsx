226077c3a1bbf224756837d93f6fd1f3f2e4a909
Camila Sanches <camilasanches.clrs@gmail.com>
2026-07-17 12:27:27 -0300

    feat: busca service orders e clients da api no dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,33 @@
+
+import type { ServiceOrder } from '../types/ServiceOrder'
+import { useState, useEffect } from 'react'
+import { api } from '../services/api'
+import type { Client } from '../types/Client'
+
+
 export const DashboardPage = () => {
+
+    const [serviceOrders, setServiceOrders] = useState<ServiceOrder[]>([]);
+    const  [loading, setLoading] = useState(true);
+    const [clients, setClients] = useState<Client[]>([]);
+
+    useEffect(() => {
+        async function searchData() {
+             const response = await api.get('/api/service-orders')
+             const clientsResponse = await api.get('/api/clients')
+             setServiceOrders(response.data)
+             setClients(clientsResponse.data)
+             setLoading(false)
+        }
+
+        searchData()
+    }, [])
+    
+    if(loading) {
+        return(
+            <p>Loading...</p>
+        )
+    }
     return (
         <div>
             <h1>Dashboard</h1>

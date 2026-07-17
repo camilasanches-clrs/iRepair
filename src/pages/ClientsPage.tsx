70623b6cfed8187f9b4eb4dd3bf4167683209d40
Camila Sanches <camilasanches.clrs@gmail.com>
2026-07-17 15:53:53 -0300

    feat: adiciona fetch clients

--- src/pages/ClientsPage.tsx
+++ src/pages/ClientsPage.tsx
@@ -1,4 +1,20 @@
+import type { Client } from "../types/Client";
+import { useState } from "react";
+import { api } from "../services/api";
+import { useEffect } from "react";
+
 export const ClientsPage = () =>{
+    const [clients, setClients] = useState<Client[]>([]);
+
+    useEffect(() => {
+        async function fetchClients() {
+            const response = await api.get('/api/clients');
+            setClients(response.data);
+        }
+
+        fetchClients();
+    }, []);
+    
     return(
         <div>
             <h1>Clients</h1>

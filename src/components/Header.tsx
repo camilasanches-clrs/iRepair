f55b364d126885cc0a2c8f0e138496c89e88d259
Camila Sanches <camilasanches.clrs@gmail.com>
2026-07-18 14:48:51 -0300

    refactor: remove componente nao utilizado e ajusta header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 
-export function Header() {
+export const Header = () => {
     return (
         <header className="bg-gray-800 text-white p-4 mb-4 rounded">
             <h1 className="flex justify-center items-center text-xl font-bold">iRepair</h1>

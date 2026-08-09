4755d2c6e10e5622e4b403953ccff672c0860f16
Camila Sanches <camilasanches.clrs@gmail.com>
2026-08-09 13:32:33 -0300

    feat: login com JWT e cookie httpOnly

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -13,3 +13,16 @@ export async function register(req: Request, res: Response): Promise<void> {
         res.status(500).json({ message: "Erro ao registrar usuário", error });
     }
 }
+
+export async function login(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.loginUser(email,password);
+        res.cookie("token", token, { httpOnly: true, maxAge: 15 * 60 * 1000 });
+        res.status(200).json({ message: "Login realizado com sucesso" });
+    }
+    catch (error) {
+        res.status(401).json({ message: "Credenciais inválidas", error });
+    }
+}
